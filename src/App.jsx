865458e442bca88c8722095174a8b6dddd745a7b
mohankumar008 <sdmohankumar06@gmail.com>
2026-08-09 09:55:26 +0530

    fixed css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Registration from "./Registration";
 import Subplans from "./Subplans";
@@ -17,23 +19,23 @@ function App() {
     };
     handlestyr();
     window.addEventListener("token-update", handlestyr);
-    return () => {
-      window.removeEventListener("token-update", handlestyr);
-    };
+    return () => window.removeEventListener("token-update", handlestyr);
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Registration />} />
-      <Route path="/plan" element={<Subplans />} />
-      <Route path="/login" element={<NextLogin />} />
-      <Route path="/payment" element={<Payment />} />
-      <Route
-        path="/fileupload"
-        element={count ? <Fileupload /> : <Navigate to="/error" />}
-      />
-      <Route path="/error" element={<Error />} />
-    </Routes>
+    <div className="app-shell">
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="/plan" element={<Subplans />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/login" element={<NextLogin />} />
+        <Route
+          path="/fileupload"
+          element={count ? <Fileupload /> : <Navigate to="/error" />}
+        />
+        <Route path="/error" element={<Error />} />
+      </Routes>
+    </div>
   );
 }
 
